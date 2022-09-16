2b6d8d89ff43c8d1067e1134febaf2de791aff5f
Anatolii Pismennyi <105397905+Giperion317@users.noreply.github.com>
2022-09-17 01:15:42 +0200

    Update galery-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+console.log(galleryItems)
+
+const galery = document.querySelector(".gallery")
+
+function galeryCreate (galleryItems) {
+    return galleryItems.map(({ preview, original, description } = element) =>
+        `<div class="gallery__item">
+  <a class="gallery__link" href="${original}" >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`).join("");
+}
+galery.insertAdjacentHTML("beforeend", galeryCreate(galleryItems));
+
+galery.addEventListener("click", onGaleryImgClick);
+
+function onGaleryImgClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    console.log(event)
+}
