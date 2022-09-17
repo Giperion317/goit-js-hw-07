bef0580229802a6e4ecb2bdae82946336acf0194
Anatolii Pismennyi <105397905+Giperion317@users.noreply.github.com>
2022-09-17 16:24:37 +0200

    Update homework-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems)
+console.log(galleryItems);
 
-const galery = document.querySelector(".gallery")
+const galery = document.querySelector('.gallery');
 
-function galeryCreate (galleryItems) {
-    return galleryItems.map(({ preview, original, description } = element) =>
+galery.addEventListener('click', onGaleryImgClick);
+
+galery.insertAdjacentHTML('beforeend', galeryCreate(galleryItems));
+
+function galeryCreate(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
         `<div class="gallery__item">
   <a class="gallery__link" href="${original}" >
     <img
@@ -16,16 +22,15 @@ function galeryCreate (galleryItems) {
       alt="${description}"
     />
   </a>
-</div>`).join("");
+</div>`
+    )
+    .join('');
 }
-galery.insertAdjacentHTML("beforeend", galeryCreate(galleryItems));
-
-galery.addEventListener("click", onGaleryImgClick);
 
 function onGaleryImgClick(event) {
-    event.preventDefault();
-    if (event.target.nodeName !== "IMG") {
-        return;
-    }
-    console.log(event)
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  console.log(event);
 }
